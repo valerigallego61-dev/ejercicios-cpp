724e2bf233db4e67ad023a4b40261a60385189e9
valerigallego61-dev <valerigallego61@gmail.com>
2026-05-27 17:57:39 -0500

    Create ejercicio 3.cpp

---
+++ taller final/ejercicio 3.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+int vector[20];
+int x;
+
+//---------------------------------------------------
+void ingresar()
+{
+	system("cls");
+	cout << "=== INGRESO DE DATOS ===" << endl;
+	cout << "========================" << endl;
+	for(x=0;x<10;x++)
+	{
+		cout << " Ingrese vector[" << x << "]: ";
+		cin >> vector[x];
+	}
+}
+
+//---------------------------------------------------
+void ver()
+{
+	system("cls");
+	cout << "datos del Vector" << endl;
+	cout << "================" << endl;
+	for(x=0;x<10;x++)
+	{
+		cout << " |vector[" << x << "] = " << vector[x];
+		cout << "|" << endl;
+	}
+	getch();
+}
+
+//---------------------------------------------------
+void proceso()
+{
+	system("cls");
+	cout << endl << endl;
+	
+	// Orden de ingreso
+	cout << " Valores en orden de ingreso:" << endl;
+	cout << "==============================" << endl;
+	for(x=0;x<10;x++)
+	{
+		cout << " " << vector[x];
+		if(x < 19) cout << ",";
+	}
+	cout << endl << endl;
+	
+	// Orden de reves
+	cout << " Valores en orden de rreves:" << endl;
+	cout << "============================" << endl;
+	for(x = 19;x >=0;x--)   // empieza desde la ultima posicion
+		//Lo importante es el for al revés, que empieza desde la posición 19 y va bajando hasta 0:
+	{
+		cout << " " << vector[x];
+		if(x> 0) cout << ",";
+	}
+	
+	cout << endl;
+	getch();
+}
+
+//-----------------------------------------------------------
+int main()
+{
+	ingresar();
+	ver();
+	proceso();
+}
